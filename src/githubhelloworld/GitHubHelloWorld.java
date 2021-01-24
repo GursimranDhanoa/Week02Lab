babb272d9843235d30a7d3bc9a6f5add69172918
821214 <821214@SAIT225178IT.ACDM.DS.SAIT.CA>
2021-01-23 18:00:03 -0700

    Coding Coded a system.out line for github

---
+++ src/githubhelloworld/GitHubHelloWorld.java
@@ -0,0 +1,27 @@
+/*
+Program: .java      Last Date of this revision: ,2020
+
+Purpose:
+
+Author: Gur Dhanoa
+*/
+
+
+package githubhelloworld;
+
+import java.util.*;
+public class GitHubHelloWorld {
+
+   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Hello Github World!");
+    }
+
+}
+
+/* Sample Output
+
+
+
+*/
